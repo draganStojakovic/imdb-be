86b9a153351efbe3123ad5c80c8249b38787eacb
qouillyan <stojakovicdragan98@gmail.com>
2023-02-27 12:20:41 +0100

    User schema

---
+++ src/app/database/schemas/User.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    confirmPassword: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('Users', userSchema);
