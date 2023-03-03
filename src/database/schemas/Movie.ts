6c0e2fd810be68aa45e4bd705a6b59fac9d838ec
qouillyan <stojakovicdragan98@gmail.com>
2023-03-03 11:15:03 +0100

    movie entity

---
+++ src/database/schemas/Movie.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { IMovie } from 'types/IMovie';
+
+const movieSchema = new mongoose.Schema<IMovie>(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    coverImage: { type: String, required: true },
+    genre: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Movie = mongoose.model('Movies', movieSchema);
---
+++ src/types/IMovie.ts
@@ -0,0 +1,6 @@
+export interface IMovie {
+  title: string;
+  description: string;
+  coverImage: string;
+  genre: string;
+}
