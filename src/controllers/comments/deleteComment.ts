b2aa7cdf4b8def01a9a2edc536bbf3f377bfe69d
qouillyan <stojakovicdragan98@gmail.com>
2023-04-04 11:39:24 +0200

    delete comment route using request body insted of query now

--- src/controllers/comments/deleteComment.ts
+++ src/controllers/comments/deleteComment.ts
@@ -4,7 +4,7 @@ import { Movie } from 'database/schemas/Movie';
 import { sanitizeError } from 'util/sanitizers';
 
 export const deleteComment = async (req: Request, res: Response) => {
-  const { commentId, movieId, userId } = req.query;
+  const { commentId, movieId, userId } = req.body;
 
   if (req.session.user._id !== userId) {
     return res
@@ -18,7 +18,7 @@ export const deleteComment = async (req: Request, res: Response) => {
       { _id: movieId },
       { $pull: { comments: commentId } }
     );
-    return res.status(204).json();
+    return res.status(200).json({ delete: 'ok' });
   } catch (e) {
     console.log(e);
     return res.status(500).json(sanitizeError('Server Error'));
--- src/validators/comments_validators/deleteCommentValidator.ts
+++ src/validators/comments_validators/deleteCommentValidator.ts
@@ -1,34 +1,34 @@
-import { query } from 'express-validator';
+import { body } from 'express-validator';
 import { Comment } from 'database/schemas/Comment';
 import { Movie } from 'database/schemas/Movie';
 import { User } from 'database/schemas/User';
 
 const deleteCommentValidator = [
-  query('commentId')
+  body('commentId')
     .exists({ checkFalsy: true })
-    .withMessage('commentId query is required')
+    .withMessage('commentId body is required')
     .isString()
-    .withMessage('commentId query must be string type')
+    .withMessage('commentId body must be string type')
     .custom(async (commentId: string) => {
       const commentExists = await Comment.exists({ _id: commentId });
       if (!commentExists) throw new Error('Comment not found');
       return true;
     }),
-  query('movieId')
+  body('movieId')
     .exists({ checkFalsy: true })
-    .withMessage('movieId query is required')
+    .withMessage('movieId body is required')
     .isString()
-    .withMessage('movieId query must be string type')
+    .withMessage('movieId body must be string type')
     .custom(async (movieId: string) => {
       const movieExists = await Movie.exists({ _id: movieId });
       if (!movieExists) throw new Error('Movie not found');
       return true;
     }),
-  query('userId')
+  body('userId')
     .exists({ checkFalsy: true })
-    .withMessage('movieId query is required')
+    .withMessage('movieId body is required')
     .isString()
-    .withMessage('movieId query must be string type')
+    .withMessage('movieId body must be string type')
     .custom(async (userId: string) => {
       const userExists = await User.exists({ _id: userId });
       if (!userExists) throw new Error('User not found');
